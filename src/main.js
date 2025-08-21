9cfa7cf87e1346e8066dd46d9b2723e11bd17305
Neil <ziembaneil@gmail.com>
2025-08-21 08:35:20 -0400

    added basic functionality to get spotify playlist and track data using spotify API

--- src/main.js
+++ src/main.js
@@ -89,11 +89,25 @@ document.getElementById("profile-pic-banner")?.addEventListener("click", () => {
 });
 
 // ========== Find Match ==========
-document.getElementById('findMatchButton')?.addEventListener('click', () => {
+document.getElementById('findMatchButton')?.addEventListener('click', async () => {
+  const playlistUrl = document.getElementById('playlistUrl').value;
+  if (!playlistUrl) {
+    console.warn('No playlist URL provided');
+    return;
+  }
+
+  const playlistData = await getPlaylistFromUrl(playlistUrl, currentToken.access_token);
+  console.log('Client validated playlist:', playlistData?.name, playlistData?.id);
+  console.log('Playlist data:', playlistData);
+
+  const trackData = await getTrackDataFromPlaylist(playlistData);
+  console.log('Track data:', trackData);
+
   fetch('http://127.0.0.1:3000/findmatch', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-    credentials: 'include'
+    credentials: 'include',
+    body: JSON.stringify(trackData)
   }).then(res => {
     if (!res.ok) return res.json().then(e => { throw new Error(e.error); });
     return res.json();
@@ -259,4 +273,50 @@ async function getUserData() {
   });
 
   return await response.json();
+}
+
+async function getPlaylistFromUrl(playlistUrl, accessToken) {
+  const id = extractPlaylistId(playlistUrl);
+  const res = await fetch(`https://api.spotify.com/v1/playlists/${id}`, {
+    headers: { 'Authorization': `Bearer ${accessToken}`}
+  });
+  return res.json();
+}
+
+async function getTrackFromId(id, accessToken) {
+  const res = await fetch(`https://api.spotify.com/v1/audio-features/${id}`, {
+    headers: { 'Authorization': `Bearer ${accessToken}`}
+  });
+  return res.json();
+}
+
+function extractPlaylistId(input) {
+  // spotify URI handler
+  const mUri = input.match(/^spotify:playlist:([A-Za-z0-9]+)$/i);
+  if (mUri) return mUri[1];
+
+  // spotify URL handler
+  const url = new URL(input);
+  const parts = url.pathname.split("/").filter(Boolean);
+  const i = parts.indexOf("playlist");
+  if (i >= 0 && parts[i + 1]) return parts[i + 1];
+
+  return null;
+}
+
+async function getTrackDataFromPlaylist(playlistData) {
+  const tracks = playlistData.tracks.items;
+  let trackData = {titles: [], artists: [], albums: []};
+
+  for (let i = 0; i < 10; i++) {
+    const currentTrack = tracks[i].track;
+    const title = currentTrack.name;
+    const artists = currentTrack.artists;
+    const album = currentTrack.album;
+    trackData.titles.push(title);
+    for (const artist of artists) { trackData.artists.push(artist.name) }
+    trackData.albums.push(album.name);
+  }
+
+  return trackData;
 }
